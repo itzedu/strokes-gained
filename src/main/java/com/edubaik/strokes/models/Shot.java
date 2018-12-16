bf51114f7c7d6221d160a4cb4bc32286723615eb
Eduardo <edubaik@gmail.com>
2018-12-15 20:48:25 -0800

    Add Shot type and distance attribute to Shot model

--- src/main/java/com/edubaik/strokes/models/Shot.java
+++ src/main/java/com/edubaik/strokes/models/Shot.java
@@ -4,5 +4,23 @@ import javax.persistence.Entity;
 
 @Entity
 public class Shot {
-
+	private String type;
+	private Integer distance;
+	
+	public Shot() {
+		
+	}
+	
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public Integer getDistance() {
+		return distance;
+	}
+	public void setDistance(Integer distance) {
+		this.distance = distance;
+	}
 }
