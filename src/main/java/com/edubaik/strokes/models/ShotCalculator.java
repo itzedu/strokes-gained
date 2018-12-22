74dbb6ba4a121e3da0344990c4c201b6a1540748
Eduardo <edubaik@gmail.com>
2018-12-21 20:39:23 -0800

    Add calculateShot method to ShotCalculator model

--- src/main/java/com/edubaik/strokes/models/ShotCalculator.java
+++ src/main/java/com/edubaik/strokes/models/ShotCalculator.java
@@ -2,4 +2,20 @@ package com.edubaik.strokes.models;
 
 public class ShotCalculator {
 	private String shotType;
+	
+	public ShotCalculator() {
+		
+	}
+	
+	public static double calculateShot() {
+		return 1.0;
+	}
+
+	public String getShotType() {
+		return shotType;
+	}
+
+	public void setShotType(String shotType) {
+		this.shotType = shotType;
+	}
 }
