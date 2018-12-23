62f2dd56c5d86d31b51dab3e44e4fdbc10c4a9b9
Eduardo <edubaik@gmail.com>
2018-12-22 17:32:26 -0800

    Add ShotCalculator Class and ApiService

---
+++ src/main/java/com/edubaik/strokes/controllers/ShotCalculatorCtrl.java
@@ -0,0 +1,8 @@
+package com.edubaik.strokes.controllers;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ShotCalculatorCtrl {
+
+}
---
+++ src/main/java/com/edubaik/strokes/services/ApiService.java
@@ -0,0 +1,8 @@
+package com.edubaik.strokes.services;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ApiService {
+
+}
